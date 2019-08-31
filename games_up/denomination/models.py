383d66b164eeb2d8c586c454e885ea023dd9f795
GaryTheStTrooper <GaryTheStTrooper@users.noreply.github.com>
2019-08-30 21:05:39 -0500

    cambios denomination

--- games_up/denomination/models.py
+++ games_up/denomination/models.py
@@ -2,28 +2,75 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
-
+import random, itertools
 
 author = 'Your name here'
 
 doc = """
-Your app description
+Denomination game
 """
 
 
 class Constants(BaseConstants):
     name_in_url = 'denomination'
     players_per_group = None
+    # Dinero con el que inician el juego
+    dotacion = 5
+    # Escojan el número de rondas aquí abajo
     num_rounds = 1
+    # Definiremos aquí el costo de los bienes ofrecidos
+    precio1 = 2
+    precio2 = 3
+
 
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(self):
 
+        # En cada ronda, se aleatorizará el rol (si recibirán 5 soles en duro o en varias monedas)
+        # Si quieren que los roles permanezcan fijos, quiten las comillas del siguiente código en lugar
+        # del código actual de este método
+        """
+        if self.round_number == 1:
+        'Añadir una tabulación a las lineas siguientes'
+        """
+        players = self.get_players()
+        random.shuffle(players)
+        treatments = itertools.cycle(['Soles', 'Centavos'])
+        for p in players:
+            # Esta es una variable auxiliar que nos permite realizar la asignación aleatoria de roles, pero no la
+            # registra en la base de datos creada por otree
+            p.participant.vars['rol'] = treatments.next()
 
 class Group(BaseGroup):
-    pass
+    def set_payoffs(self):
+        players = self.get_players()
+        for p in players:
+            # Payoff es una variable predefinida en otree para asignar los pagos por ronda o finales
+            # Aquí la defino como la dotacion recibida menos el gasto
+            p.payoff = Constants.dotacion - p.gasto
 
 
 class Player(BasePlayer):
-    pass
+    # Para que puedan ver quién está en cada tratamiento, crearemos una variable (field en otree) que lo registre en
+    # nuestros datos (igualaremos su valor al de la variable auxiliar)
+    rol = models.StringField()
+
+    # Crearemos aquí las variables que almacenen las respuestas de la encuesta inicial
+    # Si una pregunta implica una pregunta con opciones, créenla de esta forma (RadioSelect es un widget
+    # para mostrar a las personas las respuestas como alternativas de opción múltiple)
+    sexo = models.StringField(
+        choices=['Masculino', 'Femenino'],
+        label='What is your gender?',
+        widget=widgets.RadioSelect)
+    # Si implica respuestas numéricas (Enteras). Usar FloatField si quieren con decimales
+    edad = models.IntegerField()
+
+    # Crearemos aquí las variables que almacenen el número de pedidos de los bienes que quieren comprar
+    bien1 = models.IntegerField()
+    bien2 = models.IntegerField()
+
+    # Creamos una variable que guarde lo gastado en las compras y el vuelto
+    gastos = models.FloatField(max=5, min=0)
+
+
--- games_up/denomination/pages.py
+++ games_up/denomination/pages.py
@@ -3,18 +3,37 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
+# Deben crear un archivo .html por cada pagina que quieran que se muestre al jugador y que esté registrada aquí como una
+# clase, a excepción de los waitpages
+
+class Introduction(Page):
+    # Aquí se presentan las reglas básicas que un experimento debe tener. Adjuntaré un archivo de ejemplo
     pass
 
+class Instructions(Page):
+    # Aquí se mostrarán sus instrucciones
+    pass
 
-class ResultsWaitPage(WaitPage):
+class Encuesta(Page):
+    form_model = 'player'
+    # Aquí escriben todas las variables que representan las respuestas que quieren de sus jugadores
+    form_fields = ['sexo', 'edad']
 
+class CompraEspera(WaitPage):
+    # Página que hará esperar a los jugadores que hayan terminado la encuesta hasta que los demás la hayan llenado.
+    # Si no quieren que los jugadores esperen a los demás antes de pasar a la siguiente parte, borren esta clase y de
+    # la lista page_sequence
     def after_all_players_arrive(self):
         pass
 
+class Compra(Page):
+    form_model = 'player'
+    form_fields = ['bien1', 'bien2']
 
 class Results(Page):
-    pass
+    def vars_for_template(self):
+        vuelto = self.player.payoff
+        return {'vuelto': vuelto}
 
 
 page_sequence = [
